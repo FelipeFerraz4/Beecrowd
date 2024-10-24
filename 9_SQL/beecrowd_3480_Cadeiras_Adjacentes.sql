9266b1f51ba727dea26e98a81037170faf9e86af
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-10-23 22:08:58 -0300

    beecrowd 3480

---
+++ 9_SQL/beecrowd_3480_Cadeiras_Adjacentes.sql
@@ -0,0 +1,1 @@
+SELECT chairs.queue, chairs.id as "left", chairs2.id as "right" FROM chairs JOIN chairs chairs2 ON chairs.queue = chairs2.queue AND chairs.id = chairs2.id - 1 WHERE chairs.available = TRUE AND chairs2.available = TRUE ORDER BY "left";
