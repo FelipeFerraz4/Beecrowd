fd7b9bab6d69fc698676326eab84f7a9f23f8296
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-10-28 14:02:49 -0300

    refactor: beecrowd 1170 melhoria

--- 5_Matematica/beecrowd_1170_python_Blobs.py
+++ 5_Matematica/beecrowd_1170_python_Blobs.py
@@ -1,8 +1,7 @@
+import math
+
 number_tests = int(input())
-for item in range(number_tests):
+for _ in range(number_tests):
     number = float(input())
-    days = 0
-    while number > 1:
-        number /= 2
-        days += 1
+    days = math.ceil(math.log2(number))
     print(f'{days} dias')
