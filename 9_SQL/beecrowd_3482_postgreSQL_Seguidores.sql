cd9b0c1105980fd56ccceaa5ec676ee6bfa67363
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-10-29 13:09:27 -0300

    feat: beecrowd 1323

--- 9_SQL/beecrowd_3482_postgreSQL_Seguidores.sql
+++ 9_SQL/beecrowd_3482_postgreSQL_Seguidores.sql
@@ -1,3 +1,31 @@
+SELECT
+  LEAST(u1.user_name, u2.user_name) AS u1_name,
+  GREATEST(u1.user_name, u2.user_name) AS u2_name
+FROM
+  followers f
+  JOIN users u1 ON f.user_id_fk = u1.user_id
+  JOIN users u2 ON f.following_user_id_fk = u2.user_id
+WHERE
+  (u1.user_id, u2.user_id) IN (
+    SELECT
+      user_id_fk AS user_id1,
+      following_user_id_fk AS user_id2
+    FROM
+      followers
+    
+    UNION ALL
+    
+    SELECT
+      following_user_id_fk AS user_id1,
+      user_id_fk AS user_id2
+    FROM
+      followers
+  )
+  AND u1.posts < u2.posts
+ORDER BY
+  u1.user_id;
+
+/*
 WITH mutual_followers AS (
   SELECT
     followers1.user_id_fk AS follower_id,
@@ -14,7 +42,7 @@ INNER JOIN users users1 ON mutual_followers.follower_id = users1.user_id
 INNER JOIN users users2 ON mutual_followers.following_id = users2.user_id
 WHERE users1.posts < users2.posts
 ORDER BY users1.user_id;
-
+*/
 /*
 WITH mutual_followers AS (
   SELECT
