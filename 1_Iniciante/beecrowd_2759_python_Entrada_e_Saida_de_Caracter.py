b6845c8cd9377bb235082e91a967cad7e3156a34
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-11-14 21:34:35 -0300

    feat: beecrowd 2759

---
+++ 1_Iniciante/beecrowd_2759_python_Entrada_e_Saida_de_Caracter.py
@@ -0,0 +1,7 @@
+letter_a = input()
+letter_b = input()
+letter_c = input()
+
+print(f'A = {letter_a}, B = {letter_b}, C = {letter_c}')
+print(f'A = {letter_b}, B = {letter_c}, C = {letter_a}')
+print(f'A = {letter_c}, B = {letter_a}, C = {letter_b}')
